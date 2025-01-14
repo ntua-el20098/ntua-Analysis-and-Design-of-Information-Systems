e4aa92a72cff7ea8a0b3c4d7248ddade72cc41ab
CharalamposKampougeris <81620539+ntua-el20098@users.noreply.github.com>
2025-01-14 11:30:32 +0200

    Update Q35.sql

--- docker-presto-integration/Queries/Q35.sql
+++ docker-presto-integration/Queries/Q35.sql
@@ -28,7 +28,7 @@ WITH customer_sales AS (
         AND cd.cd_demo_sk = c.c_current_cdemo_sk
         AND EXISTS (
             SELECT 1
-            FROM store_sales ss, date_dim d1
+            FROM postgresql.public.store_sales ss, postgresql.public.date_dim d1
             WHERE ss.ss_customer_sk = c.c_customer_sk
               AND ss.ss_sold_date_sk = d1.d_date_sk
               AND d1.d_year = 2000  -- Replaced YEAR with 2000
@@ -36,7 +36,7 @@ WITH customer_sales AS (
         )
         AND EXISTS (
             SELECT 1
-            FROM web_sales ws, date_dim d2
+            FROM postgresql.public.web_sales ws, postgresql.public.date_dim d2
             WHERE ws.ws_bill_customer_sk = c.c_customer_sk
               AND ws.ws_sold_date_sk = d2.d_date_sk
               AND d2.d_year = 2000  -- Replaced YEAR with 2000
@@ -44,7 +44,7 @@ WITH customer_sales AS (
         )
         AND EXISTS (
             SELECT 1
-            FROM catalog_sales cs, date_dim d3
+            FROM mongodb.sf1.catalog_sales cs, postgresql.public.date_dim d3
             WHERE cs.cs_ship_customer_sk = c.c_customer_sk
               AND cs.cs_sold_date_sk = d3.d_date_sk
               AND d3.d_year = 2000  -- Replaced YEAR with 2000
