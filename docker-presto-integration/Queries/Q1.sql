8f95a1e12c1773165aab6670a8c5dc79fcabe972
CharalamposKampougeris <81620539+ntua-el20098@users.noreply.github.com>
2025-01-14 11:13:26 +0200

    Update Q1.sql

--- docker-presto-integration/Queries/Q1.sql
+++ docker-presto-integration/Queries/Q1.sql
@@ -6,9 +6,9 @@ WITH customer_total_return AS (
         sr_store_sk AS ctr_store_sk,
         SUM(sr_fee) AS ctr_total_return
     FROM 
-        store_returns
+        mongodb.sf1.store_returns
     JOIN 
-        date_dim ON sr_returned_date_sk = d_date_sk
+        postgresql.public.date_dim ON sr_returned_date_sk = d_date_sk
     WHERE 
         d_year = 2000
     GROUP BY 
@@ -18,8 +18,8 @@ SELECT
     c_customer_id
 FROM 
     customer_total_return ctr1,
-    store s,
-    customer c
+    mongodb.sf1.store s,
+    cassanda.keyspace_sf1.customer c
 WHERE 
     s.s_store_sk = ctr1.ctr_store_sk
     AND ctr1.ctr_customer_sk = c.c_customer_sk
