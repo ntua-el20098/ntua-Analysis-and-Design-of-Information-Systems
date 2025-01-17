0ca3b6a86cd0fd6c5a37bb4df1802a572fee1793
CharalamposKampougeris <81620539+ntua-el20098@users.noreply.github.com>
2025-01-17 19:33:35 +0200

    Update Q77.sql

--- docker-presto-integration/Queries/Q77.sql
+++ docker-presto-integration/Queries/Q77.sql
@@ -1,105 +1,129 @@
-WITH ss AS
- (SELECT s_store_sk,
-                        SUM(ss_ext_sales_price) AS sales,
-                        SUM(ss_net_profit) AS profit
- FROM postgresql.public.store_sales,
-               postgresql.public.date_dim,
-               mongodb.sf1.store
- WHERE ss_sold_date_sk = d_date_sk
-                AND DATE_TRUNC('day', d_date) BETWEEN DATE_TRUNC('day', DATE '1998-08-04') 
-                                                   AND (DATE_TRUNC('day', DATE '1998-08-04') + INTERVAL '30' DAY) 
-                AND ss_store_sk = s_store_sk
- GROUP BY s_store_sk)
- ,
- sr AS
- (SELECT s_store_sk,
-                        SUM(sr_return_amt) AS returns,
-                        SUM(sr_net_loss) AS profit_loss
- FROM mongodb.sf1.store_returns,
-               postgresql.public.date_dim,
-               mongodb.sf1.store
- WHERE sr_returned_date_sk = d_date_sk
-                AND DATE_TRUNC('day', d_date) BETWEEN DATE_TRUNC('day', DATE '1998-08-04')
-                                                   AND (DATE_TRUNC('day', DATE '1998-08-04') + INTERVAL '30' DAY)
-                AND sr_store_sk = s_store_sk
- GROUP BY s_store_sk), 
- cs AS
- (SELECT cs_call_center_sk,
-                       SUM(cs_ext_sales_price) AS sales,
-                       SUM(cs_net_profit) AS profit
- FROM mongodb.sf1.catalog_sales,
-               postgresql.public.date_dim
- WHERE cs_sold_date_sk = d_date_sk
-                AND DATE_TRUNC('day', d_date) BETWEEN DATE_TRUNC('day', DATE '1998-08-04')
-                                                   AND (DATE_TRUNC('day', DATE '1998-08-04') + INTERVAL '30' DAY)
- GROUP BY cs_call_center_sk 
- ), 
- cr AS
- (SELECT cr_call_center_sk,
-                        SUM(cr_return_amount) AS returns,
-                        SUM(cr_net_loss) AS profit_loss
- FROM postgresql.public.catalog_returns,
-               postgresql.public.date_dim
- WHERE cr_returned_date_sk = d_date_sk
-                AND DATE_TRUNC('day', d_date) BETWEEN DATE_TRUNC('day', DATE '1998-08-04')
-                                                   AND (DATE_TRUNC('day', DATE '1998-08-04') + INTERVAL '30' DAY)
- GROUP BY cr_call_center_sk
- ), 
- ws AS
- ( SELECT wp_web_page_sk,
-                       SUM(ws_ext_sales_price) AS sales,
-                       SUM(ws_net_profit) AS profit
- FROM postgresql.public.web_sales,
-               postgresql.public.date_dim,
-               cassandra.keyspace_sf1.web_page
- WHERE ws_sold_date_sk = d_date_sk
-                AND DATE_TRUNC('day', d_date) BETWEEN DATE_TRUNC('day', DATE '1998-08-04')
-                                                   AND (DATE_TRUNC('day', DATE '1998-08-04') + INTERVAL '30' DAY)
-                AND ws_web_page_sk = wp_web_page_sk
- GROUP BY wp_web_page_sk), 
- wr AS
- (SELECT wp_web_page_sk,
-                       SUM(wr_return_amt) AS returns,
-                       SUM(wr_net_loss) AS profit_loss
- FROM cassandra.keyspace_sf1.web_returns,
-               postgresql.public.date_dim,
-               cassandra.keyspace_sf1.web_page
- WHERE wr_returned_date_sk = d_date_sk
-                AND DATE_TRUNC('day', d_date) BETWEEN DATE_TRUNC('day', DATE '1998-08-04')
-                                                   AND (DATE_TRUNC('day', DATE '1998-08-04') + INTERVAL '30' DAY)
-                AND wr_web_page_sk = wp_web_page_sk
- GROUP BY wp_web_page_sk)
-  SELECT  channel
-                       , id
-                       , SUM(sales) AS sales
-                       , SUM(returns) AS returns
-                       , SUM(profit) AS profit
- FROM 
- (SELECT 'store channel' AS channel
-                       , ss.s_store_sk AS id
-                       , sales
-                       , COALESCE(returns, 0) AS returns
-                       , (profit - COALESCE(profit_loss,0)) AS profit
- FROM   ss LEFT JOIN sr
-                       ON  ss.s_store_sk = sr.s_store_sk
- UNION ALL
- SELECT 'catalog channel' AS channel
-                       , cs_call_center_sk AS id
-                       , sales
-                       , returns
-                       , (profit - profit_loss) AS profit
- FROM  cs
-                , cr
- UNION ALL
- SELECT 'web channel' AS channel
-                       , ws.wp_web_page_sk AS id
-                       , sales
-                       , COALESCE(returns, 0) returns
-                       , (profit - COALESCE(profit_loss,0)) AS profit
- FROM   ws LEFT JOIN wr
-                       ON  ws.wp_web_page_sk = wr.wp_web_page_sk
- ) x
- GROUP BY ROLLUP (channel, id)
- ORDER BY channel
-                        ,id
- LIMIT 100
+WITH ss AS (
+    SELECT 
+        s_store_sk,
+        SUM(ss_ext_sales_price) AS sales,
+        SUM(ss_net_profit) AS profit
+    FROM 
+        postgresql.public.store_sales,
+        postgresql.public.date_dim,
+        mongodb.sf1.store
+    WHERE 
+        ss_sold_date_sk = d_date_sk
+        AND DATE_TRUNC('day', CAST(d_date AS DATE)) BETWEEN CAST(DATE '1998-08-04' AS DATE) 
+                                                        AND (CAST(DATE '1998-08-04' AS DATE) + INTERVAL '30' DAY)
+        AND ss_store_sk = s_store_sk
+    GROUP BY s_store_sk
+),
+sr AS (
+    SELECT 
+        s_store_sk,
+        SUM(sr_return_amt) AS returns,
+        SUM(sr_net_loss) AS profit_loss
+    FROM 
+        mongodb.sf1.store_returns,
+        postgresql.public.date_dim,
+        mongodb.sf1.store
+    WHERE 
+        sr_returned_date_sk = d_date_sk
+        AND DATE_TRUNC('day', CAST(d_date AS DATE)) BETWEEN CAST(DATE '1998-08-04' AS DATE) 
+                                                        AND (CAST(DATE '1998-08-04' AS DATE) + INTERVAL '30' DAY)
+        AND sr_store_sk = s_store_sk
+    GROUP BY s_store_sk
+),
+cs AS (
+    SELECT 
+        cs_call_center_sk,
+        SUM(cs_ext_sales_price) AS sales,
+        SUM(cs_net_profit) AS profit
+    FROM 
+        mongodb.sf1.catalog_sales,
+        postgresql.public.date_dim
+    WHERE 
+        cs_sold_date_sk = d_date_sk
+        AND DATE_TRUNC('day', CAST(d_date AS DATE)) BETWEEN CAST(DATE '1998-08-04' AS DATE) 
+                                                        AND (CAST(DATE '1998-08-04' AS DATE) + INTERVAL '30' DAY)
+    GROUP BY cs_call_center_sk
+),
+cr AS (
+    SELECT 
+        cr_call_center_sk,
+        SUM(cr_return_amount) AS returns,
+        SUM(cr_net_loss) AS profit_loss
+    FROM 
+        postgresql.public.catalog_returns,
+        postgresql.public.date_dim
+    WHERE 
+        cr_returned_date_sk = d_date_sk
+        AND DATE_TRUNC('day', CAST(d_date AS DATE)) BETWEEN CAST(DATE '1998-08-04' AS DATE) 
+                                                        AND (CAST(DATE '1998-08-04' AS DATE) + INTERVAL '30' DAY)
+    GROUP BY cr_call_center_sk
+),
+ws AS (
+    SELECT 
+        wp_web_page_sk,
+        SUM(ws_ext_sales_price) AS sales,
+        SUM(ws_net_profit) AS profit
+    FROM 
+        postgresql.public.web_sales,
+        postgresql.public.date_dim,
+        cassandra.keyspace_sf1.web_page
+    WHERE 
+        ws_sold_date_sk = d_date_sk
+        AND DATE_TRUNC('day', CAST(d_date AS DATE)) BETWEEN CAST(DATE '1998-08-04' AS DATE) 
+                                                        AND (CAST(DATE '1998-08-04' AS DATE) + INTERVAL '30' DAY)
+        AND ws_web_page_sk = wp_web_page_sk
+    GROUP BY wp_web_page_sk
+),
+wr AS (
+    SELECT 
+        wp_web_page_sk,
+        SUM(wr_return_amt) AS returns,
+        SUM(wr_net_loss) AS profit_loss
+    FROM 
+        cassandra.keyspace_sf1.web_returns,
+        postgresql.public.date_dim,
+        cassandra.keyspace_sf1.web_page
+    WHERE 
+        wr_returned_date_sk = d_date_sk
+        AND DATE_TRUNC('day', CAST(d_date AS DATE)) BETWEEN CAST(DATE '1998-08-04' AS DATE) 
+                                                        AND (CAST(DATE '1998-08-04' AS DATE) + INTERVAL '30' DAY)
+        AND wr_web_page_sk = wp_web_page_sk
+    GROUP BY wp_web_page_sk
+)
+SELECT 
+    channel,
+    id,
+    SUM(sales) AS sales,
+    SUM(returns) AS returns,
+    SUM(profit) AS profit
+FROM (
+    SELECT 
+        'store channel' AS channel,
+        ss.s_store_sk AS id,
+        sales,
+        COALESCE(returns, 0) AS returns,
+        (profit - COALESCE(profit_loss, 0)) AS profit
+    FROM ss 
+    LEFT JOIN sr ON ss.s_store_sk = sr.s_store_sk
+    UNION ALL
+    SELECT 
+        'catalog channel' AS channel,
+        cs_call_center_sk AS id,
+        sales,
+        COALESCE(returns, 0) AS returns,
+        (profit - COALESCE(profit_loss, 0)) AS profit
+    FROM cs 
+    LEFT JOIN cr ON cs.cs_call_center_sk = cr.cr_call_center_sk
+    UNION ALL
+    SELECT 
+        'web channel' AS channel,
+        ws.wp_web_page_sk AS id,
+        sales,
+        COALESCE(returns, 0) AS returns,
+        (profit - COALESCE(profit_loss, 0)) AS profit
+    FROM ws 
+    LEFT JOIN wr ON ws.wp_web_page_sk = wr.wp_web_page_sk
+) x
+GROUP BY ROLLUP (channel, id)
+ORDER BY channel, id
+LIMIT 100
