db3a5731a1ab2ba382af3b2c3356817cb19527d2
CharalamposKampougeris <81620539+ntua-el20098@users.noreply.github.com>
2025-02-08 16:32:16 +0200

    Update tpcds_to_mongodb.sql

--- docker-presto-integration/tpcds_to_mongodb.sql
+++ docker-presto-integration/tpcds_to_mongodb.sql
@@ -1,7 +1,10 @@
 -- insert to mongodb
 
+-- -- Set schema name
+-- SET schema_name = 'sf1';
+
 CREATE TABLE call_center AS
-SELECT 
+SELECT
     cc_call_center_sk,
     CAST(cc_call_center_id AS VARCHAR) AS cc_call_center_id,
     cc_rec_start_date,
@@ -31,8 +34,8 @@ SELECT
     CAST(cc_state AS VARCHAR) AS cc_state,
     CAST(cc_zip AS VARCHAR) AS cc_zip,
     CAST(cc_country AS VARCHAR) AS cc_country,
-    cc_gmt_offset,
-    cc_tax_percentage
+    CAST(cc_gmt_offset AS DOUBLE) AS cc_gmt_offset, 
+    CAST(cc_tax_percentage AS DOUBLE) AS cc_tax_percentage 
 FROM tpcds.sf1.call_center;
 
 CREATE TABLE catalog_page AS
@@ -48,9 +51,74 @@ SELECT
     CAST(cp_type AS VARCHAR) AS cp_type
 FROM tpcds.sf1.catalog_page;
 
-CREATE TABLE catalog_returns AS SELECT * FROM tpcds.sf1.catalog_returns;
+CREATE TABLE catalog_returns AS
+SELECT
+    cr_returned_date_sk,
+    cr_returned_time_sk,
+    cr_item_sk,
+    cr_refunded_customer_sk,
+    cr_refunded_cdemo_sk,
+    cr_refunded_hdemo_sk,
+    cr_refunded_addr_sk,
+    cr_returning_customer_sk,
+    cr_returning_cdemo_sk,
+    cr_returning_hdemo_sk,
+    cr_returning_addr_sk,
+    cr_call_center_sk,
+    cr_catalog_page_sk,
+    cr_ship_mode_sk,
+    cr_warehouse_sk,
+    cr_reason_sk,
+    cr_order_number,
+    cr_return_quantity,
+    CAST(cr_return_amount AS DOUBLE) AS cr_return_amount, -- Cast DECIMAL to DOUBLE
+    CAST(cr_return_tax AS DOUBLE) AS cr_return_tax, -- Cast DECIMAL to DOUBLE
+    CAST(cr_return_amt_inc_tax AS DOUBLE) AS cr_return_amt_inc_tax, -- Cast DECIMAL to DOUBLE
+    CAST(cr_fee AS DOUBLE) AS cr_fee, -- Cast DECIMAL to DOUBLE
+    CAST(cr_return_ship_cost AS DOUBLE) AS cr_return_ship_cost, -- Cast DECIMAL to DOUBLE
+    CAST(cr_refunded_cash AS DOUBLE) AS cr_refunded_cash, -- Cast DECIMAL to DOUBLE
+    CAST(cr_reversed_charge AS DOUBLE) AS cr_reversed_charge, -- Cast DECIMAL to DOUBLE
+    CAST(cr_store_credit AS DOUBLE) AS cr_store_credit, -- Cast DECIMAL to DOUBLE
+    CAST(cr_net_loss AS DOUBLE) AS cr_net_loss -- Cast DECIMAL to DOUBLE
+FROM tpcds.sf1.catalog_returns;
 
-CREATE TABLE catalog_sales AS SELECT * FROM tpcds.sf1.catalog_sales;
+CREATE TABLE catalog_sales AS
+SELECT
+    cs_sold_date_sk,
+    cs_sold_time_sk,
+    cs_ship_date_sk,
+    cs_bill_customer_sk,
+    cs_bill_cdemo_sk,
+    cs_bill_hdemo_sk,
+    cs_bill_addr_sk,
+    cs_ship_customer_sk,
+    cs_ship_cdemo_sk,
+    cs_ship_hdemo_sk,
+    cs_ship_addr_sk,
+    cs_call_center_sk,
+    cs_catalog_page_sk,
+    cs_ship_mode_sk,
+    cs_warehouse_sk,
+    cs_item_sk,
+    cs_promo_sk,
+    cs_order_number,
+    cs_quantity,
+    CAST(cs_wholesale_cost AS DOUBLE) AS cs_wholesale_cost,
+    CAST(cs_list_price AS DOUBLE) AS cs_list_price,
+    CAST(cs_sales_price AS DOUBLE) AS cs_sales_price,
+    CAST(cs_ext_discount_amt AS DOUBLE) AS cs_ext_discount_amt,
+    CAST(cs_ext_sales_price AS DOUBLE) AS cs_ext_sales_price,
+    CAST(cs_ext_wholesale_cost AS DOUBLE) AS cs_ext_wholesale_cost,
+    CAST(cs_ext_list_price AS DOUBLE) AS cs_ext_list_price,
+    CAST(cs_ext_tax AS DOUBLE) AS cs_ext_tax,
+    CAST(cs_coupon_amt AS DOUBLE) AS cs_coupon_amt,
+    CAST(cs_ext_ship_cost AS DOUBLE) AS cs_ext_ship_cost,
+    CAST(cs_net_paid AS DOUBLE) AS cs_net_paid,
+    CAST(cs_net_paid_inc_tax AS DOUBLE) AS cs_net_paid_inc_tax,
+    CAST(cs_net_paid_inc_ship AS DOUBLE) AS cs_net_paid_inc_ship,
+    CAST(cs_net_paid_inc_ship_tax AS DOUBLE) AS cs_net_paid_inc_ship_tax,
+    CAST(cs_net_profit AS DOUBLE) AS cs_net_profit
+FROM tpcds.sf1.catalog_sales;
 
 CREATE TABLE customer AS
 SELECT 
@@ -75,7 +143,7 @@ SELECT
 FROM tpcds.sf1.customer;
 
 CREATE TABLE customer_address AS
-SELECT 
+SELECT
     ca_address_sk,
     CAST(ca_address_id AS VARCHAR) AS ca_address_id,
     CAST(ca_street_number AS VARCHAR) AS ca_street_number,
@@ -87,7 +155,7 @@ SELECT
     CAST(ca_state AS VARCHAR) AS ca_state,
     CAST(ca_zip AS VARCHAR) AS ca_zip,
     CAST(ca_country AS VARCHAR) AS ca_country,
-    ca_gmt_offset,
+    CAST(ca_gmt_offset AS DOUBLE) AS ca_gmt_offset,
     CAST(ca_location_type AS VARCHAR) AS ca_location_type
 FROM tpcds.sf1.customer_address;
 
@@ -136,7 +204,13 @@ SELECT
     CAST(d_current_year AS VARCHAR) AS d_current_year
 FROM tpcds.sf1.date_dim;
 
-CREATE TABLE dbgen_version AS SELECT * FROM tpcds.sf1.dbgen_version;
+CREATE TABLE dbgen_version AS
+SELECT
+    dv_version,
+    dv_create_date,
+    CAST(dv_create_time AS VARCHAR) AS dv_create_time,
+    dv_cmdline_args
+FROM tpcds.sf1.dbgen_version;
 
 CREATE TABLE household_demographics AS
 SELECT 
@@ -152,14 +226,14 @@ CREATE TABLE income_band AS SELECT * FROM tpcds.sf1.income_band;
 CREATE TABLE inventory AS SELECT * FROM tpcds.sf1.inventory;
 
 CREATE TABLE item AS
-SELECT 
+SELECT
     i_item_sk,
     CAST(i_item_id AS VARCHAR) AS i_item_id,
     i_rec_start_date,
     i_rec_end_date,
     CAST(i_item_desc AS VARCHAR) AS i_item_desc,
-    i_current_price,
-    i_wholesale_cost,
+    CAST(i_current_price AS DOUBLE) AS i_current_price,
+    CAST(i_wholesale_cost AS DOUBLE) AS i_wholesale_cost,
     i_brand_id,
     CAST(i_brand AS VARCHAR) AS i_brand,
     i_class_id,
@@ -178,13 +252,13 @@ SELECT
 FROM tpcds.sf1.item;
 
 CREATE TABLE promotion AS
-SELECT 
+SELECT
     p_promo_sk,
     CAST(p_promo_id AS VARCHAR) AS p_promo_id,
     p_start_date_sk,
     p_end_date_sk,
     p_item_sk,
-    p_cost,
+    CAST(p_cost AS DOUBLE) AS p_cost,
     p_response_targe,
     CAST(p_promo_name AS VARCHAR) AS p_promo_name,
     CAST(p_channel_dmail AS VARCHAR) AS p_channel_dmail,
@@ -218,7 +292,7 @@ SELECT
 FROM tpcds.sf1.ship_mode;
 
 CREATE TABLE store AS
-SELECT 
+SELECT
     s_store_sk,
     CAST(s_store_id AS VARCHAR) AS s_store_id,
     s_rec_start_date,
@@ -246,13 +320,60 @@ SELECT
     CAST(s_state AS VARCHAR) AS s_state,
     CAST(s_zip AS VARCHAR) AS s_zip,
     CAST(s_country AS VARCHAR) AS s_country,
-    s_gmt_offset,
-    s_tax_precentage
+    CAST(s_gmt_offset AS DOUBLE) AS s_gmt_offset,
+    CAST(s_tax_precentage AS DOUBLE) AS s_tax_precentage
 FROM tpcds.sf1.store;
 
-CREATE TABLE store_returns AS SELECT * FROM tpcds.sf1.store_returns;
+CREATE TABLE store_returns AS
+SELECT
+    sr_returned_date_sk,
+    sr_return_time_sk,
+    sr_item_sk,
+    sr_customer_sk,
+    sr_cdemo_sk,
+    sr_hdemo_sk,
+    sr_addr_sk,
+    sr_store_sk,
+    sr_reason_sk,
+    sr_ticket_number,
+    sr_return_quantity,
+    CAST(sr_return_amt AS DOUBLE) AS sr_return_amt,
+    CAST(sr_return_tax AS DOUBLE) AS sr_return_tax,
+    CAST(sr_return_amt_inc_tax AS DOUBLE) AS sr_return_amt_inc_tax,
+    CAST(sr_fee AS DOUBLE) AS sr_fee,
+    CAST(sr_return_ship_cost AS DOUBLE) AS sr_return_ship_cost,
+    CAST(sr_refunded_cash AS DOUBLE) AS sr_refunded_cash,
+    CAST(sr_reversed_charge AS DOUBLE) AS sr_reversed_charge,
+    CAST(sr_store_credit AS DOUBLE) AS sr_store_credit,
+    CAST(sr_net_loss AS DOUBLE) AS sr_net_loss
+FROM tpcds.sf1.store_returns;
 
-CREATE TABLE store_sales AS SELECT * FROM tpcds.sf1.store_sales;
+CREATE TABLE store_sales AS
+SELECT
+    ss_sold_date_sk,
+    ss_sold_time_sk,
+    ss_item_sk,
+    ss_customer_sk,
+    ss_cdemo_sk,
+    ss_hdemo_sk,
+    ss_addr_sk,
+    ss_store_sk,
+    ss_promo_sk,
+    ss_ticket_number,
+    ss_quantity,
+    CAST(ss_wholesale_cost AS DOUBLE) AS ss_wholesale_cost,
+    CAST(ss_list_price AS DOUBLE) AS ss_list_price,
+    CAST(ss_sales_price AS DOUBLE) AS ss_sales_price,
+    CAST(ss_ext_discount_amt AS DOUBLE) AS ss_ext_discount_amt,
+    CAST(ss_ext_sales_price AS DOUBLE) AS ss_ext_sales_price,
+    CAST(ss_ext_wholesale_cost AS DOUBLE) AS ss_ext_wholesale_cost,
+    CAST(ss_ext_list_price AS DOUBLE) AS ss_ext_list_price,
+    CAST(ss_ext_tax AS DOUBLE) AS ss_ext_tax,
+    CAST(ss_coupon_amt AS DOUBLE) AS ss_coupon_amt,
+    CAST(ss_net_paid AS DOUBLE) AS ss_net_paid,
+    CAST(ss_net_paid_inc_tax AS DOUBLE) AS ss_net_paid_inc_tax,
+    CAST(ss_net_profit AS DOUBLE) AS ss_net_profit
+FROM tpcds.sf1.store_sales;
 
 CREATE TABLE time_dim AS
 SELECT 
@@ -267,6 +388,7 @@ SELECT
     CAST(t_sub_shift AS VARCHAR) AS t_sub_shift,
     CAST(t_meal_time AS VARCHAR) AS t_meal_time
 FROM tpcds.sf1.time_dim;
+
 
 CREATE TABLE warehouse AS
 SELECT 
@@ -283,7 +405,7 @@ SELECT
     CAST(w_state AS VARCHAR) AS w_state,
     CAST(w_zip AS VARCHAR) AS w_zip,
     CAST(w_country AS VARCHAR) AS w_country,
-    w_gmt_offset
+    CAST(w_gmt_offset AS DOUBLE) AS w_gmt_offset
 FROM tpcds.sf1.warehouse;
 
 CREATE TABLE web_page AS
@@ -304,9 +426,70 @@ SELECT
     wp_max_ad_count
 FROM tpcds.sf1.web_page;
 
-CREATE TABLE web_returns AS SELECT * FROM tpcds.sf1.web_returns;
+CREATE TABLE web_returns AS
+SELECT 
+    wr_returned_date_sk,
+    wr_returned_time_sk,
+    wr_item_sk,
+    wr_refunded_customer_sk,
+    wr_refunded_cdemo_sk,
+    wr_refunded_hdemo_sk,
+    wr_refunded_addr_sk,
+    wr_returning_customer_sk,
+    wr_returning_cdemo_sk,
+    wr_returning_hdemo_sk,
+    wr_returning_addr_sk,
+    wr_web_page_sk,
+    wr_reason_sk,
+    wr_order_number,
+    wr_return_quantity,
+    CAST(wr_return_amt AS DOUBLE) AS wr_return_amt,
+    CAST(wr_return_tax AS DOUBLE) AS wr_return_tax,
+    CAST(wr_return_amt_inc_tax AS DOUBLE) AS wr_return_amt_inc_tax,
+    CAST(wr_fee AS DOUBLE) AS wr_fee,
+    CAST(wr_return_ship_cost AS DOUBLE) AS wr_return_ship_cost,
+    CAST(wr_refunded_cash AS DOUBLE) AS wr_refunded_cash,
+    CAST(wr_reversed_charge AS DOUBLE) AS wr_reversed_charge,
+    CAST(wr_account_credit AS DOUBLE) AS wr_account_credit,
+    CAST(wr_net_loss AS DOUBLE) AS wr_net_loss
+FROM tpcds.sf1.web_returns;
 
-CREATE TABLE web_sales AS SELECT * FROM tpcds.sf1.web_sales;
+CREATE TABLE web_sales AS
+SELECT 
+    ws_sold_date_sk,
+    ws_sold_time_sk,
+    ws_ship_date_sk,
+    ws_item_sk,
+    ws_bill_customer_sk,
+    ws_bill_cdemo_sk,
+    ws_bill_hdemo_sk,
+    ws_bill_addr_sk,
+    ws_ship_customer_sk,
+    ws_ship_cdemo_sk,
+    ws_ship_hdemo_sk,
+    ws_ship_addr_sk,
+    ws_web_page_sk,
+    ws_web_site_sk,
+    ws_ship_mode_sk,
+    ws_warehouse_sk,
+    ws_promo_sk,
+    ws_order_number,
+    ws_quantity,
+    CAST(ws_wholesale_cost AS DOUBLE) AS ws_wholesale_cost,
+    CAST(ws_list_price AS DOUBLE) AS ws_list_price,
+    CAST(ws_sales_price AS DOUBLE) AS ws_sales_price,
+    CAST(ws_ext_discount_amt AS DOUBLE) AS ws_ext_discount_amt,
+    CAST(ws_ext_sales_price AS DOUBLE) AS ws_ext_sales_price,
+    CAST(ws_ext_wholesale_cost AS DOUBLE) AS ws_ext_wholesale_cost,
+    CAST(ws_ext_list_price AS DOUBLE) AS ws_ext_list_price,
+    CAST(ws_ext_tax AS DOUBLE) AS ws_ext_tax,
+    CAST(ws_ext_ship_cost AS DOUBLE) AS ws_ext_ship_cost,
+    CAST(ws_net_paid AS DOUBLE) AS ws_net_paid,
+    CAST(ws_net_paid_inc_tax AS DOUBLE) AS ws_net_paid_inc_tax,
+    CAST(ws_net_paid_inc_ship AS DOUBLE) AS ws_net_paid_inc_ship,
+    CAST(ws_net_paid_inc_ship_tax AS DOUBLE) AS ws_net_paid_inc_ship_tax,
+    CAST(ws_net_profit AS DOUBLE) AS ws_net_profit
+FROM tpcds.sf1.web_sales;
 
 CREATE TABLE web_site AS
 SELECT 
@@ -314,26 +497,26 @@ SELECT
     CAST(web_site_id AS VARCHAR) AS web_site_id,
     web_rec_start_date,
     web_rec_end_date,
-    web_name,
+    CAST(web_name AS VARCHAR) AS web_name,
     web_open_date_sk,
     web_close_date_sk,
-    web_class,
-    web_manager,
+    CAST(web_class AS VARCHAR) AS web_class,
+    CAST(web_manager AS VARCHAR) AS web_manager,
     web_mkt_id,
-    web_mkt_class,
-    web_mkt_desc,
-    web_market_manager,
+    CAST(web_mkt_class AS VARCHAR) AS web_mkt_class,
+    CAST(web_mkt_desc AS VARCHAR) AS web_mkt_desc,
+    CAST(web_market_manager AS VARCHAR) AS web_market_manager,
     web_company_id,
     CAST(web_company_name AS VARCHAR) AS web_company_name,
     CAST(web_street_number AS VARCHAR) AS web_street_number,
-    web_street_name,
+    CAST(web_street_name AS VARCHAR) AS web_street_name,
     CAST(web_street_type AS VARCHAR) AS web_street_type,
     CAST(web_suite_number AS VARCHAR) AS web_suite_number,
-    web_city,
-    web_county,
+    CAST(web_city AS VARCHAR) AS web_city,
+    CAST(web_county AS VARCHAR) AS web_county,
     CAST(web_state AS VARCHAR) AS web_state,
     CAST(web_zip AS VARCHAR) AS web_zip,
-    web_country,
-    web_gmt_offset,
-    web_tax_percentage
+    CAST(web_country AS VARCHAR) AS web_country,
+    CAST(web_gmt_offset AS DOUBLE) AS web_gmt_offset,
+    CAST(web_tax_percentage AS DOUBLE) AS web_tax_percentage
 FROM tpcds.sf1.web_site;
